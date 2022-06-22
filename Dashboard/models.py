4043407a9968faea548dcf9470dcd35d9819c4ae
FievrePersil <tarek-tiko@live.fr>
2022-06-22 22:54:18 +0100

    Models commit

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,7 +1,16 @@
 from pyexpat import model
 from unicodedata import name
 from django.db import models
+from django.forms import EmailField, PasswordInput
 
 # Create your models here.
+class Client(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    slug = models.SlugField()
+    #use it later for reservations
+    #date = models.DateTimeField(auto_now_add=True)
 
-    
+def __str__(self):
+    return self.username
--- ResAutoBus/urls.py
+++ ResAutoBus/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include,re_path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from Dashboard import urls
 
 urlpatterns = [
@@ -24,3 +25,5 @@ urlpatterns = [
     path('contact/', include('Dashboard.urls')),
     path('home/', include('Dashboard.urls')),
 ]
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
